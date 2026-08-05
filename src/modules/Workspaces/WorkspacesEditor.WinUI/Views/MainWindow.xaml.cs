602eb9a8979dadda56f0eb9a16717374a7a61373
Muyuan Li (from Dev Box) <muyuanli@microsoft.com>
2026-08-05 09:12:52 +0800

    Fix Workspaces icon helper and shutdown

--- src/modules/Workspaces/WorkspacesEditor.WinUI/Views/MainWindow.xaml.cs
+++ src/modules/Workspaces/WorkspacesEditor.WinUI/Views/MainWindow.xaml.cs
@@ -184,6 +184,7 @@ namespace WorkspacesEditor
             _cancellationToken.Cancel();
             _cancellationToken.Dispose();
             (Microsoft.UI.Xaml.Application.Current as IDisposable)?.Dispose();
+            Microsoft.UI.Xaml.Application.Current.Exit();
         }
 
         private void AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
