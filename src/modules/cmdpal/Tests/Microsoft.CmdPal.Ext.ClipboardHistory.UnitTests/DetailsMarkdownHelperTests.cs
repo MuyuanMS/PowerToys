df5a8a0c5c353580cdbbc1f9257f0d366851f12a
Muyuan Li (from Dev Box) <muyuanli@microsoft.com>
2026-08-27 18:03:38 +0800

    Correct Markdown escaping test expectation

--- src/modules/cmdpal/Tests/Microsoft.CmdPal.Ext.ClipboardHistory.UnitTests/DetailsMarkdownHelperTests.cs
+++ src/modules/cmdpal/Tests/Microsoft.CmdPal.Ext.ClipboardHistory.UnitTests/DetailsMarkdownHelperTests.cs
@@ -36,7 +36,7 @@ public class DetailsMarkdownHelperTests
     [DataRow("[link](url)", "\\[link\\]\\(url\\)")]
     [DataRow("`code`", "\\`code\\`")]
     [DataRow("a > b", "a \\> b")]
-    [DataRow("<https://example.com>", "&lt;https://example.com>")]
+    [DataRow("<https://example.com>", "&lt;https://example\\.com\\>")]
     [DataRow("&copy;", "&amp;copy;")]
     public void EscapeMarkdown_EscapesMarkdownSyntaxCharacters(string input, string expected)
     {
