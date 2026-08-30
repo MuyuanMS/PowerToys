d010e67cb62839f726e6fd8bd3484b42fa3022fb
Muyuan Li (from Dev Box) <muyuanli@microsoft.com>
2026-08-30 16:09:09 +0800

    Cover indexer shell icon requests

--- src/modules/cmdpal/Tests/Microsoft.CmdPal.Ext.Indexer.UnitTests/DirectoryPageTests.cs
+++ src/modules/cmdpal/Tests/Microsoft.CmdPal.Ext.Indexer.UnitTests/DirectoryPageTests.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.CommandPalette.Extensions.Toolkit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.CmdPal.Ext.Indexer.UnitTests;
@@ -15,5 +16,16 @@ public class DirectoryPageTests
         var page = new DirectoryPage(string.Empty);
 
         Assert.AreSame(Icons.FileExplorerIcon, page.Icon);
+    }
+
+    [TestMethod]
+    public void NonEmptyPathCreatesShellIconRequest()
+    {
+        const string Path = @"C:\Files";
+        var page = new DirectoryPage(Path);
+
+        Assert.IsNotNull(page.Icon);
+        Assert.IsTrue(ShellItemIconProtocol.TryParse(page.Icon.Light.Icon, out var itemPath, out _));
+        Assert.AreEqual(Path, itemPath);
     }
 }
