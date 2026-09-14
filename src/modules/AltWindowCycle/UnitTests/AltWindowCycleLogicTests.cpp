3be2a8e40e49265dec901fcbf82fbc37960693e3
Muyuan Li (from Dev Box) <muyuanli@microsoft.com>
2026-09-14 15:19:06 +0800

    Clarify AltWindowCycle layout test name

--- src/modules/AltWindowCycle/UnitTests/AltWindowCycleLogicTests.cpp
+++ src/modules/AltWindowCycle/UnitTests/AltWindowCycleLogicTests.cpp
@@ -113,7 +113,7 @@ namespace AltWindowCycleUnitTests
             Assert::AreEqual(0, layout.pageSize);
         }
 
-        TEST_METHOD(TilePreviewAndHeaderRectsUseFlushEdgeToEdgeViewport)
+        TEST_METHOD(TilePreviewIsFlushAndHeaderIsInsetWithinTile)
         {
             const RECT work = { 0, 0, 1920, 1080 };
             const auto layout = AltWindowCycleLogic::ComputeOverlayLayout(work, 4, 1.0);
