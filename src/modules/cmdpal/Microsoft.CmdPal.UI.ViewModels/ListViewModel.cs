d730df4ed5496d67125a97278fdd7c0e60af2025
Muyuan Li (from Dev Box) <muyuanli@microsoft.com>
2026-09-17 08:27:44 +0800

    Clear failed queued activation after initialization

--- src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ListViewModel.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ListViewModel.cs
@@ -1116,7 +1116,7 @@ public partial class ListViewModel : PageViewModel, IDisposable
 
     private bool ShouldKeepPendingActivation(PendingActivation kind, ListItemViewModel? selectedItem)
     {
-        if (!IsInitialized || IsLoading || IsFetching)
+        if (IsLoading || IsFetching)
         {
             return true;
         }
